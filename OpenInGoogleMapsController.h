aa864ebc8c344b3f205a206f5c631dd47c30ff72
Todd Kerpelman <kerp@google.com>
2014-12-22 15:00:18 -0800

    Reference documentation
    Added doxygen-style comments to the OpenInGoogleMapsController.h file, as well as some nicely generated html docs, courtesy of appledoc

--- OpenInGoogleMapsController.h
+++ OpenInGoogleMapsController.h
@@ -23,133 +23,263 @@
 #import <CoreLocation/CoreLocation.h>
 #import <Foundation/Foundation.h>
 
-typedef NS_OPTIONS(NSInteger, GoogleMapsViewOptions) {
+/**
+ *  Options for some of the views you can toggle in Google Maps.
+ */
+typedef NS_OPTIONS(NSInteger, GoogleMapsViewOptions){
+  /**
+   *  Satellite view.
+   */
   kGoogleMapsViewOptionSatellite = 1 << 0,
+  /**
+   *  Show traffic information.
+   */
   kGoogleMapsViewOptionTraffic = 1 << 1,
+  /**
+   *  Show public transit routes.
+   */
   kGoogleMapsViewOptionTransit = 1 << 2
 };
 
-typedef NS_ENUM(NSInteger, GoogleMapsTravelMode) {
+/**
+ *  Defines the  method by which the user would like to travel when creating directions.
+ */
+typedef NS_ENUM(NSInteger, GoogleMapsTravelMode){
+  /**
+   *  Driving
+   */
   kGoogleMapsTravelModeDriving = 1,
+  /**
+   *  Public transit
+   */
   kGoogleMapsTravelModeTransit,
+  /**
+   *  Biking
+   */
   kGoogleMapsTravelModeBiking,
+  /**
+   *  Walking
+   */
   kGoogleMapsTravelModeWalking
 };
 
-typedef NS_ENUM(NSInteger, GoogleMapsFallback) {
+/**
+ *  Fallback strategies for what to show if the user doesn't have Google Maps installed.
+ */
+typedef NS_ENUM(NSInteger, GoogleMapsFallback){
+  /**
+   *  Do nothing else, and return NO. This is the default option.
+   */
   kGoogleMapsFallbackNone,
+  /**
+   *  Show the map in Apple's Maps app instead. Choose this option if you'd prefer to use a native
+   *  app at all times.
+   */
   kGoogleMapsFallbackAppleMaps,
+  /**
+   *  Show the map in Chrome, if available. Otherwise display the map in Google Maps within Safari.
+   *  Choose this option if it's important that the map you view is based on Google's map data.
+   */
   kGoogleMapsFallbackChromeThenSafari,
+  /**
+   *  Show the map in Chrome, if available. Otherwise display the map in Apple's Maps app. Choose
+   *  this option if you'd prefer apps that can point back to your application using the 
+   *  `x-callback-url` standard.
+   */
   kGoogleMapsFallbackChromeThenAppleMaps,
+  /**
+   *  Show the map in Google Maps in the Safari browser.
+   */
   kGoogleMapsFallbackSafari
 };
 
-
-// Helper class used to define a map when we open it in Google Maps, similar to a GMSCameraPosition.
-// Note that there's a good chance some of these properties will be nil, but either the queryString
-// or the center property should be set.
+/**
+ * Helper class used to define a map to be opened in Google Maps. Note that there's a good chance
+ * some of these properties will be nil, but either the `queryString` or the `center` property 
+ * should be set.
+ */
 @interface GoogleMapDefinition : NSObject
 
-// A query string which, if set, will be used to search for a place by name.
+/**
+ *  A query string which, if set, will be used to search for a place by name.
+ */
 @property(nonatomic, copy) NSString *queryString;
 
-// Location in lat/long. If both this and the query string are specified, this will be used as a
-// center for the search.
-// To clear this value, set it to kCLLocationCoordinate2DInvalid.
+/**
+ *  Location in lat/long. If both this and the `queryString` are specified, this will be used as a
+ *  center for the search.
+ *  To clear this value, set it to `kCLLocationCoordinate2DInvalid`.
+ */
 @property(nonatomic, assign) CLLocationCoordinate2D center;
 
-// Bitwise ORs of different viewing options, as seen above.
+/**
+ *  Bitwise ORs of different viewing options to display in the Google Maps application.
+ *
+ *  @see GoogleMapsViewOptions
+ */
 @property(nonatomic, assign) GoogleMapsViewOptions viewOptions;
 
-// Zoom level. Currently, Google Maps clamps this value from 0.0 (to show the whole Earth) to 21.0.
+/**
+ *  Zoom level. Currently, Google Maps clamps this value from 0.0 (to show the whole Earth) to 21.0.
+ */
 @property(nonatomic, assign) float zoomLevel;
 
 @end
 
-// Helper class used to define a Street View location when we open it in Google Maps,
-// Currently, a subset of GoogleMapDefinition, but we're keeping them separate in case
-// they diverge in the future.
+/**
+ *  Helper class used to define a Street View location to be opened in Google Maps,
+ *  Currently, this class is a subset of GoogleMapDefinition, but we're keeping these two classes
+ *  separate in case they diverge in the future.
+ */
 @interface GoogleStreetViewDefinition : NSObject
 
-// Location in lat/long. This is currently the only way you can display a Street View.
+/**
+ *  Location in lat/long. This is currently the only way you can define a Street View location.
+ */
 @property(nonatomic, assign) CLLocationCoordinate2D center;
 
 @end
 
 
-// A class used to define a waypoint for directions.
+/**
+ *  A class used to define a waypoint for directions.
+ */
 @interface GoogleDirectionsWaypoint: NSObject
 
-// Class helper method to create a waypoint with a search query.
+/**
+ *  Class helper method to create a waypoint with a search query.
+ *
+ *  @param queryString Query string for the waypoint.
+ *
+ *  @return A waypoint to be used in a GoogleDirectionsDefinition object.
+ */
 + (instancetype)waypointWithQuery:(NSString *)queryString;
 
-// Class helper method to create a waypoint with a coordinate location.
+/**
+ *  Class helper method to create a waypoint with a coordinate location.
+ *
+ *  @param location A coodinate in lat/long for the waypoint.
+ *
+ *  @return A waypoint to be used in a GoogleDirectionsDefinition object.
+ */
 + (instancetype)waypointWithLocation:(CLLocationCoordinate2D)location;
 
-// Lat/long of start location. If this is set and not equal to kCLLocationCoordinate2DInvalid,
-// it takes precedence over |queryString|.
+/**
+ *  Lat/long of start or end location. If this is set and not equal to
+ *  `kCLLocationCoordinate2DInvalid`, it takes precedence over `queryString`.
+ */
 @property(nonatomic, assign) CLLocationCoordinate2D location;
 
-// String of place to search for as our start location.
+/**
+ *  Query string to use to determine this waypoint. For best results, use unambiguous strings 
+ *  such as addresses ("355 Main Street, Cambridge, MA") or business search queries with unique
+ *  results ("The Exploratorium, San Francisco CA").
+ */
 @property(nonatomic, copy) NSString *queryString;
 
 @end
 
-// Helper class used to define two waypoints from which to create a set of directions in Google
-// Maps. Either the starting point or the destination must be non-nil in order to produce a
-// valid request.
+
+/**
+ *  Helper class used to define two waypoints from which to create a set of directions in Google
+ *  Maps. Either the starting point or the destination must be non-nil in order to produce a
+ *  valid request.
+ */
 @interface GoogleDirectionsDefinition : NSObject
 
-// Starting point. If this is set to nil, we will start at the user's current location.
+/**
+ *  Starting point. If this is set to `nil`, we will start at the user's current location.
+ * 
+ *  @see GoogleDirectionsWaypoint
+ */
 @property(nonatomic, strong) GoogleDirectionsWaypoint *startingPoint;
 
-// Destination. If this is set to nil, we will end at the user's current location.
+/**
+ *  Destination. If this is set to `nil`, we will end at the user's current location.
+ *
+ *  @see GoogleDirectionsWaypoint
+ */
 @property(nonatomic, strong) GoogleDirectionsWaypoint *destinationPoint;
 
-// Method of transportation.
+/**
+ *  Method of transportation for which to provide directions. If the application opened does not
+ *  support this travel type, it will fall back to providing driving directions.
+ *
+ * @see GoogleMapsTravelMode
+ */
 @property(nonatomic, assign) GoogleMapsTravelMode travelMode;
 
 @end
 
+/**
 
+ The `OpenInGoogleMapsController` class is designed to make it easy for an iOS developer to open a
+ map, show a Street View location, or show a set of directions directly in Google Maps. The class
+ supports using the `x-callback-URL` standard so that you can add a "Back to _my app_" button
+ directly within Google Maps, and supports a number of fallback strategies, so that you can
+ automatically open the map in another application if the user does not have Google Maps installed.
+ */
 @interface OpenInGoogleMapsController : NSObject
 
-// Returns the shared singleton instance.
+/**
+ *  Singleton method. Use this for making any calls against the `OpenInGoogleMapsController` class.
+ *
+ *  @return Returns the shared singleton instance.
+ */
 + (OpenInGoogleMapsController *)sharedInstance;
 
-// Set the callback URL that we'll want to use from within Google maps. If x-callback-url is not
-// supported, we will default back to a plain comgooglemaps: call. See the Google Maps URL scheme
-// documentation for more information about x-callback-url support.
+/**
+ *  The callback URL that you want Google maps (or Google Chrome) to use to redirect back to your 
+ *  application. If `x-callback-url` is not supported, this will not be used and Google Maps will
+ *  be opened with a simple `comgooglemaps:` call instead.
+ *  See the Google Maps URL scheme documentation for more information about `x-callback-url` 
+ *  support.
+ */
 @property(nonatomic, strong) NSURL *callbackURL;
 
-// If the user does not have Google Maps installed, what should we do?
-//  kGoogleMapsFallbackNone = Do nothing, return NO. (Default behavior)
-//  kGoogleMapsFallbackAppleMaps = Open the map with Apple Maps.
-//  kGoogleMapsFallbackChromeThenSafari = Open the map with Google Chrome if installed, otherwise
-//    use Safari.
-//  kGoogleMapsFallbackChromeThenAppleMaps = Open the map with Google Chrome if installed, otherwise
-//    use Apple Maps.
-//  kGoogleMapsFallbackSafari = Open the map with Safari.
+/**
+ *  Determines what to do if the user does not have Google Maps. You only need to set the
+ *  fallback strategy once during the lifetime of your application and it will be used in all future
+ *  `OpenInGoogleMapsController` requests.
+ *  @see GoogleMapsFallback
+ */
 @property(nonatomic, assign) GoogleMapsFallback fallbackStrategy;
 
-// Returns YES if Google Maps is installed.
+/**
+ *  Evaluates to `YES` if Google Mpas is installed, `NO` otherwise.
+ */
 @property(nonatomic, readonly, getter=isGoogleMapsInstalled) BOOL googleMapsInstalled;
 
-// Opens a map with the characteristics specified in the GoogleMapDefinition in Google Maps.
-// Returns YES if it was succesfully able to open the map in Google Maps or one of the fallback
-// options as specified in |fallbackStrategy|. Returns NO otherwise.
+/**
+ *  Opens a map with the characteristics specified in the `GoogleMapDefinition `in Google Maps.
+ *
+ *  @param definition A `GoogleMapDefinition` for the map you would like to open.
+ *
+ *  @return Returns `YES` if it was succesfully able to open the map in Google Maps or one of the
+ *  fallback options as specified in `fallbackStrategy`. Returns `NO` otherwise.
+ */
 - (BOOL)openMap:(GoogleMapDefinition *)definition;
 
-// Shows Street View location in Google maps.
-// Returns YES if it was successfully able to open the Google Maps app or one of the fallback
-// options as specified in |fallbackStrategy|. Does _not_ guarantee that the address entered is a
-// valid street view location.
+/**
+ *  Shows Street View location in Google maps.
+ *
+ *  @param definition A GoogleStreetViewDefinition for the location you would like to view.
+ *
+ *  @return Returns `YES` if it was successfully able to open the Google Maps app or one of the
+ *  fallback options as specified in `fallbackStrategy`. Does _not_ guarantee that the address
+ *  entered is a valid street view location.
+ */
 - (BOOL)openStreetView:(GoogleStreetViewDefinition *)definition;
 
-// Shows Directions in Google maps.
-// Returns YES if it was successfully able to open the Google Maps app or one of the fallback
-// options as specified in |fallbackStrategy|. Does _not_ guarantee that the directions request
-// is a valid one, or that directions were found.
+/**
+ * Show point-to-point directions in Google Maps.
+ *
+ * @param definition A GoogleDirectionsDefinition for the location you would like to view.
+ *
+ * @return Returns `YES` if it was successfully able to open the Google Maps app or one of the
+ * fallback options as specified in `fallbackStrategy`. Does _not_ guarantee that the directions
+ * request is a valid one, or that directions were found.
+ */
 - (BOOL)openDirections:(GoogleDirectionsDefinition *)definition;
 
 
